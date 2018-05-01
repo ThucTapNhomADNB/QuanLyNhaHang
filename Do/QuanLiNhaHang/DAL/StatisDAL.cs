5b2095c2a32de6f02afc3ae642613eebdfe4371b
thubinh <30625871+thubinh@users.noreply.github.com>
2018-05-01 21:27:40 +0700

    add thống kê doanh thu

---
+++ Do/QuanLiNhaHang/DAL/StatisDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace QuanLiNhaHang.DAL
+{
+    class StatisDAL
+    {
+        DatabaseConnect dtConnect = new DatabaseConnect();
+        public DataTable LoadDS(DateTime date1, DateTime date2)
+        {
+            string query = string.Format("SELECT * FROM dbo.Bill WHERE DateCheckIn>={0} AND DateCheckIn<={1}", date1, date2);
+            return dtConnect.GETdata(query);
+        }
+    }
+}
